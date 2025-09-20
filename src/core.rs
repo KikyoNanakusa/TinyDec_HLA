695e4b47e3129cc5020546d0285e8665d0ccd659
KikyoNanakusa <nanakusa.vrc@gmail.com>
2025-09-20 23:56:56 +0900

    Add chap3 content

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,20 @@
 use std::path::Path;
+use crate::disassemble::disassemble_file;
+use anyhow::Result;
 
-pub fn decompile(_path: &Path) -> Result<String, String>{
+pub fn decompile(_path: &Path) -> Result<String>{
+	let (symbols, instructions) = disassemble_file(_path)?;
+	for symbol in symbols {
+		println!("Symbol: {} at 0x{:x}", symbol.name, symbol.addr);
+	}
+	println!();
+	println!("Instructions: ");
+	for instruction in instructions {
+		print!("0x{:x} {} ", instruction.address, instruction.mnemonic);
+		if let Some(operands) = instruction.operands {
+			print!("{}", operands);
+		}
+		println!();
+	}
 	Ok(String::from("Hello, Decompiler!"))
 }
---
+++ src/disassemble/mod.rs
@@ -0,0 +1,2 @@
+pub mod disassemble;
+pub use disassemble::{disassemble_file};
